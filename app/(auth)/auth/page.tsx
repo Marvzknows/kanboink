8abc43d9786ceccd43e705c52319b4230aac4fc7
Marvin Lim <marvinlim@Marvins-MacBook-Air.local>
2025-08-15 21:17:54 +0800

    install prisma

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
-  return <div>pagasde</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default page;
