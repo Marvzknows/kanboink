4dac97f051df394469a46e8561447529c9d79249
Marvin Lim <marvinlim@Marvins-MacBook-Air.local>
2025-09-06 12:09:14 +0800

    authentication middleware implementation

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse, NextRequest } from "next/server";
+import * as jose from "jose";
+
+const secret = new TextEncoder().encode(process.env.JWT_ACCESS_SECRET);
+
+export default async function middleware(request: NextRequest) {
+  const token = request.cookies.get("access_token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+
+  try {
+    await jose.jwtVerify(token, secret);
+    return NextResponse.next();
+  } catch {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/projects/:path*", "/dashboard/:path*"],
+};
