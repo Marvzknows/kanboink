ca1bd128832924d83ad2c1a88c82747345aee7b1
marvin <marvzknows03@gmail.com>
2025-09-22 14:43:09 +0800

    db migration from mysql to postgresql

--- prisma/migrations/20250904072531_add_priority_to_card/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `priority` to the `Card` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Card` ADD COLUMN `priority` ENUM('LOW', 'MEDIUM', 'HIGH', 'URGENT') NOT NULL;
