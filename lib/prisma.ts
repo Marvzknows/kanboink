7afb01ca95cfb728261f7aaf1a9789df87a5b988
Marvin Lim <marvinlim@Marvins-MacBook-Air.local>
2025-09-27 22:56:52 +0800

    install prisma postgre

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,10 @@
 import { PrismaClient } from "./generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 //   new PrismaClient({
 //     log: ["query"], // optional: useful for debugging queries
 //   });
